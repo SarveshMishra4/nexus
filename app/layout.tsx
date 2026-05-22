297890e592955c33cb227a7d360f13e40a4b022f
Sarvesh Mishra <creategreat.sarvesh@gmail.com>
2026-05-22 16:50:49 +0530

    Added Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={rubik.variable}>
       <head>
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-K5Z3FTDGZP"
           strategy="afterInteractive"
@@ -75,13 +76,37 @@ export default function RootLayout({
           {`
             window.dataLayer = window.dataLayer || [];
 
-            function gtag(){
+            function gtag() {
               dataLayer.push(arguments);
             }
 
             gtag('js', new Date());
 
             gtag('config', 'G-K5Z3FTDGZP');
+          `}
+        </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1719750505873027');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
